7ab235aba387f375ce5b10b8fd793fa22b99dc5d
yarashev <farhodyarashev.uz@gmail.com>
2025-06-10 09:19:50 +0500

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,17 +1,19 @@
 // server/db.ts
-import pkg from "pg";
-const { Pool } = pkg;
 
+import "dotenv/config";
+import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "../shared/schema";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL must be set.");
+  throw new Error("DATABASE_URL environment variable is not set.");
 }
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: {
+    rejectUnauthorized: false, // Render.com yoki Railway uchun
+  },
 });
 
 export const db = drizzle(pool, { schema });
